8a6f219d84275a7eed00cb5941dd819dce7422bf
Emanuele <e.lazzari@geosystems.it>
2023-11-28 08:42:21 +0100

    Delete & Edit on profile listing section

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,5 @@
 import { getAuth, updateProfile } from "firebase/auth";
-import { collection, doc, getDocs, orderBy, query, updateDoc, where } from "firebase/firestore";
+import { collection, deleteDoc, doc, getDocs, orderBy, query, updateDoc, where } from "firebase/firestore";
 import { useEffect, useState } from "react"
 import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
@@ -73,6 +73,21 @@ export default function Profile() {
         fetchUserListings();
     }, [auth.currentUser.uid])
 
+    async function onDelete(listingID) {
+        if(window.confirm("Are you sure you want to delete?")) {
+            await deleteDoc(doc(db, "listings", listingID));
+            const updatedListings = listings.filter(
+                (listing) => listing.id !== listingID
+            );
+            setListings(updatedListings);
+            toast.success("Succesfully deleted the listing");
+        }
+    }
+
+    function onEdit(listingID) {
+        navigate(`/edit-listing/${listingID}`)
+    }
+
     return (
         <>
             <section className="max-w-6xl mx-auto flex justify-center items-center flex-col">
@@ -112,6 +127,8 @@ export default function Profile() {
                                     key={listing.id}
                                     id={listing.id}
                                     listing={listing.data}
+                                    onDelete={() => onDelete(listing.id)}
+                                    onEdit={() => onEdit(listing.id)}
                                 />
                             ))}
                         </ul>
