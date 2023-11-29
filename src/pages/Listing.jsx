e5318a1b7b25089aa744f42df2b3fc60a3f2a54a
Emanuele <e.lazzari@geosystems.it>
2023-11-29 17:31:00 +0100

    Share/copy functionality

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -6,17 +6,13 @@ import Spinner from "../components/Spinner";
 import { EffectFade, Autoplay, Navigation, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import 'swiper/css/bundle';
-
-// import { Swiper, SwiperSlide } from "swiper/react";
-// import { EffectFade, Autoplay, Navigation, Pagination } from "swiper/modules";
-// import "swiper/css/bundle";
-
+import { FaShare } from "react-icons/fa";
 
 export default function Listing() {
     const params = useParams();
     const [listing, setListing] = useState(null);
     const [loading, setLoading] = useState(true);
-    // SwiperCore.use(Autoplay, Navigation, Pagination);
+    const [shareLinkCopied, setShareLinkCopied] = useState(false);
     useEffect(() => {
         async function fetchListing() {
             const docRef = doc(db, "listings", params.listingId)
@@ -56,42 +52,19 @@ export default function Listing() {
                     </SwiperSlide>
                 ))}
             </Swiper>
+            <div 
+                className="fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer bottom-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center"
+                onClick={() => {
+                    navigator.clipboard.writeText(window.location.href)
+                    setShareLinkCopied(true);
+                    setTimeout(() => {setShareLinkCopied(false)}, 2000)
+                }}
+            >
+                <FaShare className="text-lg text-slate-500"/>
+            </div>
+            {shareLinkCopied && (
+                <p className="fixed top-[23%] right-[5%] font-semibold border-2 border-gray-400 rounded-md bg-white z-10 p-2">Link Copied</p>
+            )}
         </main>
     )
 }
-
-
-/* <main>
-    <Swiper slidesPerView={1} navigation pagination={{type: "progressbar"}} effect="fade" modules={[EffectFade]} autoplay={{delay: 3000}}>
-        {listing.imgUrls.map((url, index) => (
-            <SwiperSlide key={index}>
-                <div className="w-full overflow-hidden h-[300px]" style={{background: `url(${listing.imgUrls[index]}) center no-repeat`}}>
-
-                </div>
-            </SwiperSlide>
-        ))}
-    </Swiper>
-</main> */
-
-/*<main>
-    <Swiper
-        modules={[Navigation, Pagination, Autoplay, EffectFade]}
-        slidesPerView={1}
-        navigation
-        pagination={{ clickable: true, type: "progressbar" }}
-        effect="fade"
-        autoplay={{ delay: 3000 }}
-    >
-        {listing.imgUrls.map((url, index) => (
-            <SwiperSlide key={index}>
-                <div
-                className="relative w-full overflow-hidden h-[300px]"
-                style={{
-                    background: `url(${listing.imgUrls[index]}) center no-repeat`,
-                    backgroundSize: "cover",
-                }}
-                ></div>
-            </SwiperSlide>
-        ))}
-    </Swiper>
-</main>*/
