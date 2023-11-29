bdd9e8b9393d497801eb9315e7430bc51888c16d
Emanuele <e.lazzari@geosystems.it>
2023-11-29 17:17:36 +0100

    Listing & Swiper package

---
+++ src/pages/Listing.jsx
@@ -0,0 +1,97 @@
+import { doc, getDoc } from "firebase/firestore";
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom";
+import { db } from "../firebase";
+import Spinner from "../components/Spinner";
+import { EffectFade, Autoplay, Navigation, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import 'swiper/css/bundle';
+
+// import { Swiper, SwiperSlide } from "swiper/react";
+// import { EffectFade, Autoplay, Navigation, Pagination } from "swiper/modules";
+// import "swiper/css/bundle";
+
+
+export default function Listing() {
+    const params = useParams();
+    const [listing, setListing] = useState(null);
+    const [loading, setLoading] = useState(true);
+    // SwiperCore.use(Autoplay, Navigation, Pagination);
+    useEffect(() => {
+        async function fetchListing() {
+            const docRef = doc(db, "listings", params.listingId)
+            const docSnap = await getDoc(docRef);
+            if (docSnap.exists()) {
+                setListing(docSnap.data());
+                setLoading(false);
+                
+            }
+        }
+        fetchListing();
+    }, [params.listingId]);
+    if (loading) {
+        return < Spinner />
+    }
+
+
+    return (
+        <main>
+            <Swiper
+                modules={[Navigation, Pagination, Autoplay, EffectFade]}
+                slidesPerView={1}
+                navigation
+                pagination={{ clickable: true, type: "progressbar" }}
+                effect="fade"
+                autoplay={{ delay: 3000 }}
+            >
+                {listing.imgUrls.map((url, index) => (
+                    <SwiperSlide key={index}>
+                        <div
+                        className="relative w-full overflow-hidden h-[300px]"
+                        style={{
+                            background: `url(${listing.imgUrls[index]}) center no-repeat`,
+                            backgroundSize: "cover",
+                        }}
+                        ></div>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        </main>
+    )
+}
+
+
+/* <main>
+    <Swiper slidesPerView={1} navigation pagination={{type: "progressbar"}} effect="fade" modules={[EffectFade]} autoplay={{delay: 3000}}>
+        {listing.imgUrls.map((url, index) => (
+            <SwiperSlide key={index}>
+                <div className="w-full overflow-hidden h-[300px]" style={{background: `url(${listing.imgUrls[index]}) center no-repeat`}}>
+
+                </div>
+            </SwiperSlide>
+        ))}
+    </Swiper>
+</main> */
+
+/*<main>
+    <Swiper
+        modules={[Navigation, Pagination, Autoplay, EffectFade]}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true, type: "progressbar" }}
+        effect="fade"
+        autoplay={{ delay: 3000 }}
+    >
+        {listing.imgUrls.map((url, index) => (
+            <SwiperSlide key={index}>
+                <div
+                className="relative w-full overflow-hidden h-[300px]"
+                style={{
+                    background: `url(${listing.imgUrls[index]}) center no-repeat`,
+                    backgroundSize: "cover",
+                }}
+                ></div>
+            </SwiperSlide>
+        ))}
+    </Swiper>
+</main>*/
