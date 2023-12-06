bb41b76cf61c4c8bfa6ca26e087c5f4ce2776b12
Emanuele <e.lazzari@geosystems.it>
2023-12-06 09:11:32 +0100

    Slider component in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,10 @@
-import { useEffect } from "react";
 import Slider from "../components/Slider";
 
 export default function Home() {
-    useEffect(() => {
-        async function fetchListings () {
-            
-        }
-        fetchListings();
-    }, [])
 
     return (
         <div>
-            <Slider></Slider>
+            < Slider />
         </div>
     )
 }
